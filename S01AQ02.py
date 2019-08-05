0bfc3f98f77f1c5b8a8701cd4a915f7a144d6837
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2019-08-06 00:18:26 +0530

    Add files via upload

---
+++ S01AQ02.py
@@ -0,0 +1,12 @@
+def multi_table():
+    num = int(input("enter the number : "))
+    
+    for i in range(1,11):
+         number = num*i
+         print(num,'x',i,'=',number)
+   
+
+
+
+#main starts here
+multi_table()
