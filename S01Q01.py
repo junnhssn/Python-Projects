7199324880dd1215158e2c69b416761f993bc746
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2019-08-05 18:58:09 +0530

    Add files via upload

---
+++ S01Q01.py
@@ -0,0 +1,11 @@
+def user_name():
+    res = input("Enter your name : ")
+    return res
+
+
+
+
+
+#main starts from here
+name = user_name()
+print("Hello", name)
---
+++ S01Q03.py
@@ -0,0 +1,10 @@
+def multi_table():
+    number = num*i
+    return number
+
+
+
+#main starts here
+num = int(input("enter the number : "))
+for i in range(1,11):
+    print(num,'x',i,'=',multi_table())
