8d0257db7a4ca6d7968a05db613ca370ac531ecb
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2019-08-18 01:18:30 +0530

    Add files via upload

--- Q12.py
+++ Q12.py
@@ -13,33 +13,46 @@ def science_theory():
 def science_practical():
     sci_pra  = int(input("Enter your Science practical marks : "))
     return sci_pra
+   
     
+def class_declaration(p,m,e,s1,s2):
 
+
+    if 90 <= p < 100 and m > 35 and e > 25 and s1 > 8  and s2 > 25 :
+        print("Congratulations, you have obtained an A grade with", p,"%")
+
+
+
+    elif 75 <= p < 90 and m > 35 and e > 25 and s1 > 8  and s2 > 25 :
+        print("Congratulations, you have obtained a B grade with", p,"%")
+
+
+    elif 35 <= p < 75 and m > 35 and e > 25 and s1 > 8  and s2 > 25 :
+        print("Congratulations, you have obtained a B grade with", p,"%")
+
+    else :
+        print("Unfortunately you have failed")
+
+
+
+def percentage(m,e,s1,s2):
+    
+    total_marks = 275 
+    total_scored = m + e + s1 + s2
+    percentage = (total_scored/total_marks)*100
+    return percentage
+
+    
 def main():
-    total_marks = 275
     math = math_score()
     english = english_score()
     science_1 = science_practical()
     science_2 = science_theory()
-    total_scored = math + english + science_1 + science_2
-    percentage = (total_scored/total_marks)*100
-    if 90 <= percentage < 100 and math > 35 and english > 25 and science_1> 8  and science_2 >25 :
-        print("Congratulations, you have obtained an A grade with", percentage,"%")
+    percentage(math,english,science_1,science_2)
+    cent = percentage(math,english,science_1,science_2)
+    class_declaration(cent,math,english,science_1,science_2)
         
-        
-        
-    elif 75 <= percentage < 90 and math > 35 and english > 25 and science_1> 8  and science_2 >25 :
-        print("Congratulations, you have obtained a B grade with", percentage,"%")
-        
-        
-    elif 35 < percentage < 75 and math > 35 and english > 25 and science_1> 8  and science_2 >25 :
-        print("Congratulations, you have obtained a B grade with", percentage,"%")
-        
-    else :
-        print("Unfortunately you have failed")
-        
-        
-#main starts here        
+#main starts here       
 main()       
         
         
