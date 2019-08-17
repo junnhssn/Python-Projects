72467646b7e57c0d5f9cb6cdefd6bb4817dad4af
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2019-08-17 18:44:55 +0530

    Add files via upload

---
+++ Q13.py
@@ -0,0 +1,12 @@
+import math
+
+def user_input():
+    while True:
+        res = int(input('Enter a Three digit number : '))
+        if res == 0:
+            break
+        if 100 <= res <= 1000:
+            print(math.sqrt(res))
+        else:
+            print("Invalid input!! Please enter a positive three digit number")
+user_input()
